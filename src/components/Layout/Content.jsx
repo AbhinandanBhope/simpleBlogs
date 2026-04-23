aa78419b0ac5823ff6a3b0208bbbeeee1c9e9306
AbhinandanBhope <abhinandanbhope1642000@gmail.com>
2026-04-23 12:57:39 +0530

    Working on design

--- src/components/Layout/Content.jsx
+++ src/components/Layout/Content.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import  '../../styles/CSS/Content.css'
 function Content() {
+
+   const [items, setItems] = useState([
+    {
+      title: "Burger",
+      summery: "lorem loerem lorem",
+      price: "10.99"
+    },
+    {
+      title: "Pizza",
+      summery: "Cheesy pizza loaded with toppings",
+      price: "12.99"
+    }
+  ]);
+
+
   return (
      <div className="content">
         <div className='box'> 
             <div className="title">Delicious Burger 🍔</div>
             <div className="summery">Freshly grilled burger with crispy lettuce, cheese, and juicy patty.</div>
+              
 
         </div>
+        <div className="items">
+           {items.map((item, index) => (
+          <div key={index}>
+            <h2>{item.title}</h2>
+            <p>{item.summery}</p>
+            <h3>${item.price}</h3>
+            <hr className="line" />
+          </div>
+        ))}
+          
+        </div>
+        
       
     </div>
   )
